822e9b139924e88661f59d0955527f376f6c7e74
BG-Home-Official <bghomeofficial@gmail.com>
2025-09-21 08:33:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const ApiKey = [
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
-	const { history, message } = req.body;
+	const { history, message, system} = req.body;
 	
 	if (!message) {
 		return res.status(400).json({ error: "No message provided" });
@@ -39,6 +39,7 @@ app.post("/chat", async (req, res) => {
 			const response = await ai.models.generateContent({
 				model: "gemini-2.0-flash-001",
 				contents: chatHistory,
+				systemInstruction: system ? { role: "system", parts: [{ text: system }] } : undefined,
 			});
 			
 			reply = response.text;
